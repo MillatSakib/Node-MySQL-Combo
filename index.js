0fa9a612a5e4c8bc2a8d7fecc34030b3567d4d37
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-10-04 12:07:44 +0600

    Database connection completed

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+})
+
+con.connect((err) => {
+    if (err) {
+        throw err;
+
+    } else {
+        console.log("Database Connected");
+    }
+})
+
+//configure dotenv
+dotenv.config();
 
 //rest object 
 const app = express();
 
-//middlewares
+//middleware
+app.use(express.json());
 
 //routes
 app.get("/test", (req, res) => {
-    res.status(200).send("<h1>Nodejs Mysql server is running.</h1>")
+    con.query("CREATE DATABASE vugijugi", (err, result) => {
+        if (err) console.log(err);
+        res.status(200).send("<h1>Nodejs Mysql server is running.Also Created a database</h1>");
+    })
+
 })
 
 //port 
-const port = 8080
+const port = process.env.PORT;
 
 
 //listen
 app.listen(port, () => {
-    console.log(`Server is running " + port + ". Test at http://localhost:${port}/test`);
+    console.log(`Server is running ${port}. Test at http://localhost:${port}/test`);
 })
+
+
